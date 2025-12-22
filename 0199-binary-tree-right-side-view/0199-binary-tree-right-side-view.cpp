cfc1a6e17ee755b0e30543e35dbab81de8f28cc6
Aryan Tyagi <aryant214@gmail.com>
2025-12-22 17:27:34 +0530

    Time: 0 ms (100%), Space: 14.9 MB (84.73%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,7 +11,20 @@
  */
 class Solution {
 public:
+     void dsf(TreeNode* root, int dep, vector<int>& res) {
+        if (!root)
+            return;
+
+        if (dep == res.size())
+            res.push_back(root->val);
+
+        dsf(root->right, dep + 1, res);
+        dsf(root->left, dep + 1, res);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
-        
+        vector<int> res;
+        dsf(root, 0, res);
+        return res;
     }
 };
