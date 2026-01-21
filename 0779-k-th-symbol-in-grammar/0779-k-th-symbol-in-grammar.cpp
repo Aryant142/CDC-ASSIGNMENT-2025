b5233bffba5567c54f0853a125ceaa7958732f97
Aryan Tyagi <aryant214@gmail.com>
2026-01-21 22:41:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.09%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int n , int k){
+        
+        if(n == 1 && k == 1){
+            return 0;
+        }
+        int length = pow(2,n-1);
+        int mid = length/2;
+        if(mid >= k){
+           return solve(n-1,k);
+        }
+         return 1-solve(n-1,k-mid);
+    }
+    int kthGrammar(int n, int k) {
+        int ans = solve(n,k);
+        return ans;
+    }
+};
